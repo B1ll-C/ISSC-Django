2f9d3250277051e28704af62e2b636b27231921c
Paul Joshua Gonzalodo <wh04m1.b@gmail.com>
2025-02-08 00:15:15 +0800

    Template added

--- issc/main/urls.py
+++ issc/main/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="dashboard"),
+    path("incidents/", views.index, name="incidents"),
+    path("vehicles/", views.index, name="vehicles"),
+    path("live_feed/", views.index, name="live_feed"),
+    path("about/", views.index, name="about"),
+    path("logout/", views.index, name="logout"),
 ]
