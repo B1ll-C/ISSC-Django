a93ab4bf854d2df05df11a78831f90a0719c9e1e
Paul Joshua Gonzalodo <wh04m1.b@gmail.com>
2025-02-10 01:11:51 +0800

    models registered

--- issc/main/admin.py
+++ issc/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import AccountRegistration, VehicleRegistration, IncidentReport
 # Register your models here.
+admin.site.register(AccountRegistration)
+admin.site.register(VehicleRegistration)
+admin.site.register(IncidentReport)
