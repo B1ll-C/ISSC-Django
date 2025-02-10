b602f05a8721325b12887f7c099ec799f786865c
Paul Joshua Gonzalodo <wh04m1.b@gmail.com>
2025-02-10 21:40:17 +0800

    signup route and form handling

--- issc/main/views.py
+++ issc/main/views.py
@@ -7,9 +7,9 @@ from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
 
+from .models import AccountRegistration
 
-user_roles = ['admin','student','faculty']
-i = 0
+
 
 
 def login(request):
@@ -20,8 +20,8 @@ def login(request):
     if request.method == 'POST':
         email = request.POST['email']
         password = request.POST['password']
-        print(email)
-        print(password)
+        # print(email)
+        # print(password)
         user = authenticate(request, username=email,password=password)
         if user:
             auth_login(request, user)
@@ -29,53 +29,110 @@ def login(request):
 
     return HttpResponse(template.render(context,request))
 
+
+
 @login_required(login_url='/login/')
 def signup(request):
+    user = AccountRegistration.objects.filter(username=request.user).values()
     template = loader.get_template('signup.html')
-    context = {}
+    context = {
+        'user_role': user[0]['privilege'],
+        'user_data':user[0]
+    }
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        first_name = request.POST.get('first_name')
+        middle_name = request.POST.get('middle_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        id_number = request.POST.get('id_number')
+        contact_number = request.POST.get('contact_number')
+        gender = request.POST.get('gender')
+        department = request.POST.get('department')
+        privilege = request.POST.get('privilege')
+        status = request.POST.get('status')
+        password = request.POST.get('password')
+
+        print(password)
+
+        user = AccountRegistration(
+            username=username,
+            first_name=first_name,
+            middle_name=middle_name,
+            last_name=last_name,
+            email=email,
+            id_number=id_number,
+            contact_number=contact_number,
+            gender=gender,
+            department=department,
+            privilege=privilege,
+            status=status,
+            password=password
+        )
+
+        user.set_password(password)
+        user.save()
+        return redirect('signup')
     return HttpResponse(template.render(context,request))
+
 
 @login_required(login_url='/login/')
 def base(request):
+    user = AccountRegistration.objects.filter(username=request.user).values()
+
     if not request.user.is_authenticated:
         redirect('login')
     template = loader.get_template('dashboard/dashboard.html')
-    context = {}
+    context = {
+        'user_role': user[0]['privilege'],
+        'user_data':user[0]
+    }
+
     return HttpResponse(template.render(context, request))
 
 @login_required(login_url='/login/')
 def incident(request):
-    user_role = user_roles[i]
+    user = AccountRegistration.objects.filter(username=request.user).values()
     template = loader.get_template('incident/incident.html')
     context = {
-        'user_role': user_role
+        'user_role': user[0]['privilege'],
+        'user_data':user[0]
+
     }
     return HttpResponse(template.render(context, request))
 
 @login_required(login_url='/login/')
 def vehicles(request):
-    user_role = user_roles[i]
+    user = AccountRegistration.objects.filter(username=request.user).values()
+
     template = loader.get_template('vehicle/vehicle.html')
     context = {
-        'user_role': user_role
+        'user_role': user[0]['privilege'],
+        'user_data':user[0]
+
     }
     return HttpResponse(template.render(context, request))
 
 @login_required(login_url='/login/')
 def live_feed(request):
-    user_role = user_roles[i]
     template = loader.get_template('live-feed/live-feed.html')
     context = {
-        'user_role': user_role
+        'user_role': user[0]['privilege'],
+        'user_data':user[0]
+
     }
     return HttpResponse(template.render(context, request))
 
 @login_required(login_url='/login/')
 def about(request):
-    user_role = user_roles[i]
+    user = AccountRegistration.objects.filter(username=request.user).values()
+
     template = loader.get_template('about/about.html')
     context = {
-        'user_role': user_role
+        'user_role': user[0]['privilege'],
+        'user_data':user[0]
+
     }
     return HttpResponse(template.render(context, request))
 
