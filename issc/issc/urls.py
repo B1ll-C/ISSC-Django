bc6fd647ab28d1b0c2f1de9abd87fe093cf5c2c7
Billy <wh04m1.b@gmail.com>
2025-02-19 00:33:03 +0800

    File serving for recorded live feed

--- issc/issc/urls.py
+++ issc/issc/urls.py
@@ -27,3 +27,4 @@ urlpatterns = [
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.RECORDING_URL, document_root=settings.RECORDING_ROOT)
