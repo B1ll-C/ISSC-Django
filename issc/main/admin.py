8f1ecfd28ea55d14284c66feae6eb9c6c5b49a2d
Villaflor, Justin John Mico <justinjohnmico.09@gmail.com>
2025-05-18 03:21:13 +0800

    Register FaceLogs model in the admin interface

--- issc/main/admin.py
+++ issc/main/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import AccountRegistration, VehicleRegistration, IncidentReport, FacesEmbeddings, VehicleEntry
+from .models import AccountRegistration, VehicleRegistration, IncidentReport, FacesEmbeddings, VehicleEntry, FaceLogs
 # Register your models here.
 admin.site.register(AccountRegistration)
 admin.site.register(VehicleRegistration)
 admin.site.register(IncidentReport)
 admin.site.register(FacesEmbeddings)
 admin.site.register(VehicleEntry)
+admin.site.register(FaceLogs)
